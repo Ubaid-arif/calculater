b029d352cf58575dc6297932975f648427366784
Ubaid-arif <giftprintingservice@gmail.com>
2020-06-27 00:53:48 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function getNumber(num){
+    var result = document.getElementById("result");
+    result.value += num;
+}
+
+function clearResult(){
+    var result = document.getElementById("result");
+    result.value = " ";
+}
+function getResult(){
+    var result = document.getElementById("result");
+    result.value  = eval(result.value)
+}
